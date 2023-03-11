cec9282dfa9fbf0d3bcc22959d48664e42ecee63
Chris Harris <>
2023-03-11 20:27:39 +1030

    implemented query methods

--- src/map_reader.py
+++ src/map_reader.py
@@ -4,16 +4,43 @@ OpenStreetMap (OSM) data
 
 Author: Chris Harris
 """
+
+import requests
+
 class MapReader:
     """
     Class handles accessing OpenStreetMap data and returning relevant road/street data
     """
     def __init__(self):
-        pass
+        self._overpass_url = "http://overpass-api.de/api/interpreter"
 
-    def _get_map_data_from_bbox(self, west_longitude: float, south_latitude: float,
-                                east_longitude: float, north_latitude: float):
+    def _get_highway_data_from_bbox(self, south_latitude: float, west_longitude: float,
+                                    north_latitude: float, east_longitude: float) -> dict:
         """
-        Return all OSM nodes, ways and relations within the box defined by the latitudes/longitudes
+        Return all OSM nodes, ways and relations within the box defined by the latitudes/longitudes.
         """
-        pass
+        roi = f"({south_latitude}, {west_longitude}, {north_latitude}, {east_longitude})"
+        feature = "highway"
+
+        query = f"""
+        [out:json];
+        (
+        node["{feature}"]
+            {roi};
+        way["{feature}"]
+            {roi};
+        relation["{feature}"]
+            {roi};
+        )
+        (._;>;);
+        out center;
+        """
+        return self._query_overpass(query)
+
+    def _query_overpass(self, query: str, timeout=30) -> dict:
+        """
+        Send query to Overpass API and return json formatted result
+        """
+        response = requests.get(self._overpass_url, params={"data": query}, timeout=timeout)
+        response_data = response.json()
+        return response_data
